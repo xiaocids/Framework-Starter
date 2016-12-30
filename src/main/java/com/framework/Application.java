73e8e0cb395442a1f39ebf02d479af8650611fd6
xiaocids <xiaocids@live.com>
2016-12-30 14:34:27 +0700

    Add Security Config and DB Configuration

--- src/main/java/com/framework/Application.java
+++ src/main/java/com/framework/Application.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StarterApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(StarterApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 }
--- src/main/java/com/framework/Config/ServletInitializer.java
+++ src/main/java/com/framework/Config/ServletInitializer.java
@@ -1,5 +1,6 @@
-package com.framework;
+package com.framework.Config;
 
+import com.framework.Application;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
@@ -7,7 +8,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(StarterApplication.class);
+		return application.sources(Application.class);
 	}
 
 }
