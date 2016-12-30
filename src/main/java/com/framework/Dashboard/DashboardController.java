46f74166ed812c85c13ba119772db5fd1b52a265
xiaocids <xiaocids@live.com>
2016-12-30 14:40:53 +0700

    Add Dashboard Controller

---
+++ src/main/java/com/framework/Dashboard/DashboardController.java
@@ -0,0 +1,16 @@
+package com.framework.Dashboard;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Rosid on 12/30/2016.
+ */
+@Controller
+@RequestMapping("/Dashboard")
+public class DashboardController {
+    @RequestMapping(value = {"/","Index"})
+    public String Index(){
+        return "Dashboard/Index";
+    }
+}
